4f7e8d7e170a3d843d0e2acaf2edfa5c0b42ecb8
Soma Ando <soma.ando.1227@gmail.com>
2024-10-12 15:50:24 +0900

    Add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import HeaderWrapper from "@/components/HeaderWrapper"
 import { Footer } from "@/components/Footer"
 import { Slideshow } from "@/components/Slideshow"
 const inter = Inter({ subsets: ["latin"] });
+import GoogleAnalytics from '@/components/GoogleAnalytics/GoogleAnalytics'
 
 export const metadata: Metadata = {
   title: "香川高専 電波祭",
@@ -18,6 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja" className="tracking-widest">
+      <head>
+        <GoogleAnalytics />
+      </head>
       <body className={inter.className}>
         <HeaderWrapper />
         <Slideshow />
