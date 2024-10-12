a8809834d2ecfdf2b183472ee280770a355aeac7
Soma Ando <soma.ando.1227@gmail.com>
2024-10-12 16:43:55 +0900

    Revert "Edit Header component"
    This reverts commit 8a9a98ae8c1b1b44327cd2cb4c40cca33f9ea462.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import HeaderWrapper from "@/components/HeaderWrapper"
+import { Header } from "@/components/Header"
 import { Footer } from "@/components/Footer"
 import { Slideshow } from "@/components/Slideshow"
 const inter = Inter({ subsets: ["latin"] });
@@ -23,7 +23,7 @@ export default function RootLayout({
         <GoogleAnalytics />
       </head>
       <body className={inter.className}>
-        <HeaderWrapper />
+        <Header />
         <Slideshow />
         {children}
         <Footer />
